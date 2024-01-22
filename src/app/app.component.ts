0325bd5de1c1fcad12e2c65b105b5893cfc45546
Alexander Bearden <eabearden97@aol.com>
2024-01-21 19:42:57 -0600

    display a list of heroes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,19 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeroesComponent } from './heroes/heroes.component';
+import { Hero } from './hero';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HeroesComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'tour-of-heroes';
+  title = 'Tour of Heroes';
+  hero: Hero = {
+    id: 1,
+    name: 'Alexander'
+  };
 }
---
+++ src/app/heroes/heroes.component.ts
@@ -0,0 +1,23 @@
+import { Component } from '@angular/core';
+import { CommonModule, NgFor } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { HEROES } from '../mock-heroes';
+import { Hero } from '../hero';
+
+
+@Component({
+  selector: 'app-heroes',
+  standalone: true,
+  imports: [CommonModule, FormsModule, NgFor],
+  templateUrl: './heroes.component.html',
+  styleUrl: './heroes.component.css'
+})
+export class HeroesComponent {
+  heroes = HEROES;
+  selectedHero?: Hero;
+
+  onSelect(hero: Hero): void {
+    this.selectedHero=hero;
+  }
+
+}
