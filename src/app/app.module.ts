f0bed88ee9c65f7b817fda48359d2c111c418207
Alexander Bearden <eabearden97@aol.com>
2024-01-29 21:11:07 -0600

    add app routing

---
+++ src/app/app.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { AppRoutingModule } from './app-routing.module';
+
+@NgModule({
+  imports: [
+    AppRoutingModule
+  ],
+  declarations: [
+
+  ],
+  bootstrap: [ ]
+})
+export class AppModule { }
